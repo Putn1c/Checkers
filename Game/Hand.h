01e0ec59e8dc1c97f188113660cba7ab8ebfa817
Ilya Suvorin <syvorin@gmail.com>
2025-01-16 17:40:55 +0300

    fix

--- Game/Hand.h
+++ Game/Hand.h
@@ -1,102 +1,114 @@
-#pragma once
+﻿#pragma once
 #include <tuple>
 
 #include "../Models/Move.h"
 #include "../Models/Response.h"
 #include "Board.h"
 
-// methods for hands
+// Класс для обработки действий игрока
 class Hand
 {
-  public:
-    Hand(Board *board) : board(board)
+public:
+    Hand(Board* board) : board(board) // Конструктор класса Hand, инициализирующий указатель на объект доски
     {
     }
+
+    // Метод для получения ввода игрока (выбор клетки)
     tuple<Response, POS_T, POS_T> get_cell() const
     {
-        SDL_Event windowEvent;
-        Response resp = Response::OK;
-        int x = -1, y = -1;
-        int xc = -1, yc = -1;
-        while (true)
+        SDL_Event windowEvent; // Структура для обработки событий SDL
+        Response resp = Response::OK; // Изначальный ответ - успешный
+        int x = -1, y = -1; // Координаты мыши
+        int xc = -1, yc = -1; // Ячеечные координаты, вычисленные из экранных координат
+
+        while (true) // Бесконечный цикл для обработки ввода
         {
-            if (SDL_PollEvent(&windowEvent))
+            if (SDL_PollEvent(&windowEvent)) // Проверяем наличие событий
             {
                 switch (windowEvent.type)
                 {
-                case SDL_QUIT:
-                    resp = Response::QUIT;
+                case SDL_QUIT: // Проверка, было ли событие выхода из приложения
+                    resp = Response::QUIT; // Установка ответа на выход
                     break;
-                case SDL_MOUSEBUTTONDOWN:
-                    x = windowEvent.motion.x;
-                    y = windowEvent.motion.y;
-                    xc = int(y / (board->H / 10) - 1);
-                    yc = int(x / (board->W / 10) - 1);
+
+                case SDL_MOUSEBUTTONDOWN: // Обработка нажатий мыши
+                    x = windowEvent.motion.x; // Получаем координаты курсора по оси X
+                    y = windowEvent.motion.y; // Получаем координаты курсора по оси Y
+                    xc = int(y / (board->H / 10) - 1); // Вычисляем ячейку по вертикали
+                    yc = int(x / (board->W / 10) - 1); // Вычисляем ячейку по горизонтали
+
+                    // Проверка условий для различных действий
                     if (xc == -1 && yc == -1 && board->history_mtx.size() > 1)
                     {
-                        resp = Response::BACK;
+                        resp = Response::BACK; // Если есть история ходов, игрок хочет вернуться обратно
                     }
                     else if (xc == -1 && yc == 8)
                     {
-                        resp = Response::REPLAY;
+                        resp = Response::REPLAY; // Если нажато в области повтора игры
                     }
-                    else if (xc >= 0 && xc < 8 && yc >= 0 && yc < 8)
+                    else if (xc >= 0 && xc < 8 && yc >= 0 && yc < 8) // Проверка, находится ли клик внутри игрового поля
                     {
-                        resp = Response::CELL;
+                        resp = Response::CELL; // Игрок выбрал клетку для хода
                     }
-                    else
+                    else // Если клик не в рамках игры
                     {
-                        xc = -1;
+                        xc = -1; // Сброс координат
                         yc = -1;
                     }
                     break;
-                case SDL_WINDOWEVENT:
-                    if (windowEvent.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
+
+                case SDL_WINDOWEVENT: // Обработка событий окна
+                    if (windowEvent.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) // Проверка изменения размера окна
                     {
-                        board->reset_window_size();
+                        board->reset_window_size(); // Сброс размеров доски
                         break;
                     }
                 }
-                if (resp != Response::OK)
-                    break;
+                if (resp != Response::OK) // Если произошел выход или другое действие
+                    break; // Выход из цикла
             }
         }
-        return {resp, xc, yc};
+        return { resp, xc, yc }; // Возврат кортежа с результатом, координатами ячейки
     }
 
+    // Метод ожидания ввода от игрока
     Response wait() const
     {
-        SDL_Event windowEvent;
-        Response resp = Response::OK;
-        while (true)
+        SDL_Event windowEvent; // Структура для обработки событий SDL
+        Response resp = Response::OK; // Изначальный ответ - успешный
+        while (true) // Бесконечный цикл для обработки ввода
         {
-            if (SDL_PollEvent(&windowEvent))
+            if (SDL_PollEvent(&windowEvent)) // Проверяем наличие событий
             {
                 switch (windowEvent.type)
                 {
-                case SDL_QUIT:
-                    resp = Response::QUIT;
+                case SDL_QUIT: // Проверка, было ли событие выхода из приложения
+                    resp = Response::QUIT; // Установка ответа на выход
                     break;
-                case SDL_WINDOWEVENT_SIZE_CHANGED:
-                    board->reset_window_size();
+
+                case SDL_WINDOWEVENT_SIZE_CHANGED: // Проверка изменения размера окна
+                    board->reset_window_size(); // Сброс размеров доски
                     break;
-                case SDL_MOUSEBUTTONDOWN: {
-                    int x = windowEvent.motion.x;
-                    int y = windowEvent.motion.y;
-                    int xc = int(y / (board->H / 10) - 1);
-                    int yc = int(x / (board->W / 10) - 1);
-                    if (xc == -1 && yc == 8)
-                        resp = Response::REPLAY;
+
+                case SDL_MOUSEBUTTONDOWN: { // Обработка нажатий мыши
+                    int x = windowEvent.motion.x; // Получаем координаты курсора по оси X
+                    int y = windowEvent.motion.y; // Получаем координаты курсора по оси Y
+                    int xc = int(y / (board->H / 10) - 1); // Вычисляем ячейку по вертикали
+                    int yc = int(x / (board->W / 10) - 1); // Вычисляем ячейку по горизонтали
+
+                    if (xc == -1 && yc == 8) // Проверка нажатия на кнопку повтора
+                        resp = Response::REPLAY; // Установка ответа на повтор
                 }
-                break;
+                                        break;
                 }
-                if (resp != Response::OK)
-                    break;
+                if (resp != Response::OK) // Если произошел выход или другое действие
+                    break; // Выход из цикла
             }
         }
-        return resp;
+        return resp; // Возврат результата ввода
     }
 
-  private:
-    Board *board;
+private:
+    Board* board; // Указатель на объект доски
 };
+
